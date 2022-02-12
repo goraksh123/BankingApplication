e984e6bab6268edc7f2576040a32b652b1424fb6
goraksh123 <goraksh.naik@gmail.com>
2022-02-13 00:04:11 +0530

    Update OtherConfigration.java

--- currency/src/main/java/com/fx/trading/currency/configurations/OtherConfigration.java
+++ currency/src/main/java/com/fx/trading/currency/configurations/OtherConfigration.java
@@ -3,6 +3,7 @@ package com.fx.trading.currency.configurations;
 import com.fx.trading.currency.utils.SyncSequenceGenerator;
 import org.springframework.context.annotation.Bean;
 
+@Configuration
 public class OtherConfigration {
 
     @Bean( name="SyncSequenceGenerator")
